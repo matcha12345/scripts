00a69674b6e48c1ede3751a7150d29d12dfae45e
matcha12345 <165014253+matcha12345@users.noreply.github.com>
2025-03-01 11:09:02 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,8 @@
-sudo yum update -y
-sudo amazon-linux-extras enable corretto8
-sudo yum install -y java-17-amazon-corretto
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum install -y jenkins
-sudo systemctl start jenkins
-sudo systemctl enable jenkins
-
+sudo yum install java-17-amazon-corretto -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install jenkins -y
+systemctl start jenkins
+sytemctl enable jenkins
+systemctl status jenkins
+ 
